9651c63b59dc7aca28137777d7aefad24d392de0
erobbi <elirobbins0@gmail.com>
2021-09-13 10:31:44 -0400

    sessions for login/logout working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
 
 
 #   before_action :authorize
