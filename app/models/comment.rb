f055a7e6e47a7610eced423efb9a04528bce78ee
ellijs <ellijs@hotmail.com>
2021-09-16 13:50:10 -0400

    Add Username on Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :blog
+
+  def who_wrote_comment 
+    writer = User.find(self.user_id)
+    writer.username
+  end
 end
--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :content, :user_id
+  attributes :id, :content, :user_id, :who_wrote_comment 
   has_one :user
   has_one :blog
 end
